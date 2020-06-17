3f79ac12db8c1fe95d516d091907057bfffdb5c4
Forrest Eastland <feastland4@gmail.com>
2020-06-16 18:45:15 -0700

    added code

---
+++ client/build/precache-manifest.d67c0ef46422b7a78efc9ccf1e34fc70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "be49a5b98cbecbda5cb1",
+    "url": "/static/css/main.0fc05ddd.chunk.css"
+  },
+  {
+    "revision": "be49a5b98cbecbda5cb1",
+    "url": "/static/js/main.b40133f8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4f14e103a85ded4a2db8",
+    "url": "/static/js/2.9f08725b.chunk.js"
+  },
+  {
+    "revision": "f7784aa15024e04f8f6e29b2f542c66e",
+    "url": "/index.html"
+  }
+];
